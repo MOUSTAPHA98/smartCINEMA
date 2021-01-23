60ba605a84f34ecca1aa4d2ff92b916911f7582f
MOUSTAPHA98 <67459997+MOUSTAPHA98@users.noreply.github.com>
2021-01-23 23:46:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ let onNavItemClick = (pathName) => {
 contentDiv.innerHTML = routes[window.location.pathname];
 
 window.addEventListener("popstate", event => {
-  let stateId = "home";
+  let stateId = event.state.id;
   console.log("stateId = ", stateId);
   select_tab(stateId);
     document.title = ``;
