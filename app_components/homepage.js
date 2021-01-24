b2862ae64bee133904a9e5a9b9131a08cffe56e5
MOUSTAPHA98 <67459997+MOUSTAPHA98@users.noreply.github.com>
2021-01-24 07:49:37 +0200

    ...

--- app_components/homepage.js
+++ app_components/homepage.js
@@ -1,1 +1,198 @@
-let homepage = `HOME PAGE`;
+let homepage = `
+<!--=========================-->
+<!--    ===     HOMEPAGE AREA    ===    -->
+<!--=========================-->
+
+    <section class="homepage-area">
+        <div class="homepage-content-swiper-container">
+            <!-- Add Arrows -->
+            <div class="homepage-content-swiper-button-next swiper-button-next swiper-pagination">
+                <i class="fal fa-chevron-right"></i>
+            </div>
+            <div class="homepage-content-swiper-button-prev swiper-button-prev swiper-pagination">
+                <i class="fal fa-chevron-left"></i>
+            </div>
+            <!-- Add Pagination -->
+            <div class="homepage-content-swiper-pagination swiper-pagination"></div>
+
+            <div class="swiper-wrapper">
+
+            <!--    ===     START SLIDE #01    ===    -->
+                <div class="swiper-slide slide1">
+                    <div class="slide-content">
+                    <img class="" src="./src/images/deadpool.png" alt="">
+                        <div class="row">
+                            <div class="slide-movie col-xl-7 p-0 d-flex flex-column justify-content-center">
+                                    <span class="slide-category">
+                                        MOVIES
+                                    </span>
+                                    <h2 class="slide-movie-title">
+                                        Deadpool 2
+                                    </h2>
+                                    <div class="slide-movie-generes d-flex flex-row align-items-center">
+                                    <span>
+                                        R
+                                    </span>
+                                    <span>
+                                        action
+                                    </span>
+                                    <span>
+                                        adventure
+                                    </span>
+                                    <span>
+                                        comedy
+                                    </span>
+                                    </div>
+                                    <div class="slide-movie-info d-flex flex-row align-items-center">
+                                    <span class="slide-movie-info-rate">
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fal fa-star"></i>
+                                    </span>
+                                    <span class="slide-movie-info-likes">
+                                        <i class="fas fa-thumbs-up"></i>
+                                        31565 Like
+                                    </span>
+                                    <span class="slide-movie-info-comments">
+                                        <i class="fas fa-comment"></i>
+                                        459 Comments
+                                    </span>
+                                    </div>
+                                    <p class="slide-movie-description">
+                                        A wisecracking mercenary gets experimented on and becomes immortal but ugly, and sets out to track down the man who ruined his looks.
+                                    </p>
+                                    <div class="slide-movie-btns d-flex flex-row justify-content-start align-items-center">
+                                        <a class="btn">Watch Now</a>
+                                        <a class="btn invert">View Trailer</a>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--    ===     END SLIDE #01    ===    -->
+
+                <!--    ===     START SLIDE #02    ===    -->
+                <div class="swiper-slide slide2">
+                <div class="slide-content">
+                    <img class="" src="./src/images/ironman.png" alt="">
+                    <div class="row">
+                            <div class="slide-movie col-xl-7 p-0 d-flex flex-column justify-content-center">
+                                <span class="slide-category">
+                                    MOVIES
+                                </span>
+                                <h2 class="slide-movie-title">
+                                    Iron Man
+                                </h2>
+                                <div class="slide-movie-generes d-flex flex-row align-items-center">
+                                    <span>
+                                        PG-13
+                                    </span>
+                                    <span>
+                                        action
+                                    </span>
+                                    <span>
+                                        adventure
+                                    </span>
+                                    <span>
+                                        sci-fi
+                                    </span>
+                                </div>
+                                <div class="slide-movie-info d-flex flex-row align-items-center">
+                                    <span class="slide-movie-info-rate">
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fal fa-star"></i>
+                                        <i class="fal fa-star"></i>
+                                    </span>
+                                    <span class="slide-movie-info-likes">
+                                        <i class="fas fa-thumbs-up"></i>
+                                        31565 Like
+                                    </span>
+                                    <span class="slide-movie-info-comments">
+                                        <i class="fas fa-comment"></i>
+                                        459 Comments
+                                    </span>
+                                </div>
+                                <p class="slide-movie-description">
+                                    After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.
+                                </p>
+                                <div class="slide-movie-btns d-flex flex-row justify-content-start align-items-center">
+                                    <a class="btn">Watch Now</a>
+                                    <a class="btn invert">View Trailer</a>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <!--    ===     END SLIDE #02    ===    -->
+
+            <!--    ===     START SLIDE #03    ===    -->
+            <div class="swiper-slide slide3">
+                <div class="slide-content">
+                    <img class="" src="./src/images/marvel.png" alt="">
+                    <div class="row">
+                            <div class="slide-movie col-xl-12 p-0 d-flex flex-column justify-content-center">
+                                <span class="slide-category">
+                                    MOVIES
+                                </span>
+                                <h2 class="slide-movie-title">
+                                    Caption America
+                                </h2>
+                                <div class="slide-movie-generes d-flex flex-row align-items-center">
+                                    <span>
+                                        R
+                                    </span>
+                                    <span>
+                                        action
+                                    </span>
+                                    <span>
+                                        adventure
+                                    </span>
+                                    <span>
+                                        sci-fi
+                                    </span>
+                                </div>
+                                <div class="slide-movie-info d-flex flex-row align-items-center">
+                                    <span class="slide-movie-info-rate">
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fal fa-star"></i>
+                                        <i class="fal fa-star"></i>
+                                        <i class="fal fa-star"></i>
+                                    </span>
+                                    <span class="slide-movie-info-likes">
+                                        <i class="fas fa-thumbs-up"></i>
+                                        31565 Like
+                                    </span>
+                                    <span class="slide-movie-info-comments">
+                                        <i class="fas fa-comment"></i>
+                                        459 Comments
+                                    </span>
+                                </div>
+                                <p class="slide-movie-description">
+                                    Steve Rogers, a rejected military soldier, <br> 
+                                    transforms into Captain America after taking <br> 
+                                    a dose of a "Super-Soldier serum".
+                                </p>
+                                <div class="slide-movie-btns d-flex flex-row justify-content-start align-items-center">
+                                    <a class="btn">Watch Now</a>
+                                    <a class="btn invert">View Trailer</a>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <!--    ===     END SLIDE #03   ===    -->
+
+            </div>
+
+            <!-- Add Pagination -->
+            <div class="homepage-content-swiper-pagination swiper-pagination"></div>
+            
+        </div>
+        <!-- <br><br><br><br><br> -->
+    </section>
+`;
