ad269ba839bdfd25170d072ec5ed816b8c75c3d9
MOUSTAPHA98 <67459997+MOUSTAPHA98@users.noreply.github.com>
2021-01-26 22:17:36 +0200

    ...

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var load_content = new Function(`return GET_DATA_${location.pathname.substr(1).toUpperCase()}()`);
-load_content();
+// var load_content = new Function(`return GET_DATA_${location.pathname.substr(1).toUpperCase()}()`);
+// load_content();
 
 
 let routes = {
   '/': home,
-  '/HOME': home,
   '/index.html': home,
   '/POPULAR' : popular,
   '/NOW_PLAYING' : now_playing,
@@ -17,33 +16,39 @@ let routes = {
 
 
 
-let onNavItemClick = (pathName) => {
-  var newPath = pathName.substring(1);
-  load_content = new Function(`return GET_DATA_${newPath}()`);
-  load_content();
-}
 
 
 
+// var newPath = pathName.substring(1);
+// load_content = new Function(`return GET_DATA_${newPath}()`);
+// load_content();
 
-function select_tab(id) {
-  document.querySelectorAll(".navbar-link").forEach(item => item.classList.remove('active'));
-  document.querySelectorAll("#" + id).forEach(item => item.classList.add('active'));
-}
+
+// function select_tab(id) {
+//   document.querySelectorAll(".navbar-link").forEach(item => item.classList.remove('active'));
+//   document.querySelectorAll("#" + id).forEach(item => item.classList.add('active'));
+// };
+
+
+
 
 
 function push(event) {
 
   let id = event.target.id;
-  if (history.state === null && id != "home"){
-    window.history.pushState({id}, `${id}`, `${id.toUpperCase()}`);
-  } else if ( history.state != null && id != history.state.id ) {
-    window.history.pushState({id}, `${id}`, `${id.toUpperCase()}`);
-  } 
 
-  select_tab(id);
-  document.title = `smartCINEMA | ${id.toUpperCase()}`;
+  // load_content = new Function(`return GET_DATA_${id.toUpperCase()}()`);
+  // load_content();
+
+  // if (history.state === null && id != "home"){
+  //   window.history.pushState({id}, `${id}`, `${id.toUpperCase()}`);
+  // } else if ( history.state != null && id != history.state.id) {
+  //   window.history.pushState({id}, `${id}`, `${id.toUpperCase()}`);
+  // }
+
+  // document.title = `smartCINEMA | ${id.toUpperCase()}`;
 }
+
 
 
 window.onload = event => {
@@ -57,18 +62,18 @@ window.onload = event => {
   window["bookmarks"].addEventListener("click", event => push(event))
 }
 
-window.addEventListener("popstate", event => {
-  var stateId;
-  if (event.state === null ){
-    stateId = "home";
-    select_tab(stateId);
-      document.title = ``;
-      document.title = `smartCINEMA | HOME`;
+// window.addEventListener("popstate", event => {
+//   var stateId;
+//   if (event.state === null ){
+//     stateId = "home";
+//     select_tab(stateId);
+//       document.title = ``;
+//       document.title = `smartCINEMA | HOME`;
 
-  } else {
-       stateId = event.state.id;
-      select_tab(stateId);
-        document.title = ``;
-        document.title = `smartCINEMA | ${stateId.toUpperCase()}`;
-  }
-});
+//   } else {
+//        stateId = event.state.id;
+//       select_tab(stateId);
+//         document.title = ``;
+//         document.title = `smartCINEMA | ${stateId.toUpperCase()}`;
+//   }
+// });
