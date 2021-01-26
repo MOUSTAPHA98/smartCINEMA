0c6553ab3a56f28a9f9fa5c64f67ca33fb02cd6d
MOUSTAPHA98 <67459997+MOUSTAPHA98@users.noreply.github.com>
2021-01-27 00:11:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-let navbarLinks = document.querySelectorAll('.navbar-link');
-
-
-var pathLink = window.location.pathname.substring(1);
-var load_content = new Function (`return GET_DATA_${pathLink}()`);
-
-
-document.title = `smartCINEMA | ${pathLink.toUpperCase()}`;
-load_content();
-
-
 let routes = {
   '/': home,
+  '/home': home,
   '/index.html': home,
   '/popular' : popular,
   '/now_playing' : now_playing,
@@ -20,6 +10,15 @@ let routes = {
   '/favourites' : favourites,
   '/bookmarks' : bookmarks,
 };
+
+var pathLink = window.location.pathname.substring(1);
+document.title = `smartCINEMA | ${pathLink.toUpperCase()}`;
+
+var load_content = new Function (`return GET_DATA_${pathLink}()`);
+load_content();
+
+
+let navbarLinks = document.querySelectorAll('.navbar-link');
 
 function select_tab(id) {
   navbarLinks.forEach(item => item.classList.remove('active'));
@@ -66,65 +65,3 @@ window.addEventListener("popstate", event => {
         document.title = `smartCINEMA | ${stateId}`;
   }
 });
-
-// let onNavItemClick = (pathName) => {
-//   console.log(this)
-//   // console.log(pathName);
-//   let pathLink = pathName.substring(1);
-//   console.log(pathLink);
-//   //   window.history.pushState({id}, `${id}`, `${id.toUpperCase()}`);
-
-//   window.history.pushState({pathLink}, `${pathLink}`, `${pathLink}`);
-// }
-
-
-// var newPath = pathName.substring(1);
-// load_content = new Function(`return GET_DATA_${newPath}()`);
-// load_content();
-
-
-
-
-
-
-
-
-function push(event) {
-
-  let id = event.target.id;
-
-  // load_content = new Function(`return GET_DATA_${id.toUpperCase()}()`);
-  // load_content();
-
-
-  // document.title = `smartCINEMA | ${id.toUpperCase()}`;
-}
-
-
-
-window.onload = event => {
-  window["home"].addEventListener("click", event => push(event))
-  window["popular"].addEventListener("click", event => push(event))
-  window["now_playing"].addEventListener("click", event => push(event))
-  window["top_rated"].addEventListener("click", event => push(event))
-  window["upcoming"].addEventListener("click", event => push(event))
-  window["search"].addEventListener("click", event => push(event))
-  window["favourites"].addEventListener("click", event => push(event))
-  window["bookmarks"].addEventListener("click", event => push(event))
-}
-
-// window.addEventListener("popstate", event => {
-//   var stateId;
-//   if (event.state === null ){
-//     stateId = "home";
-//     select_tab(stateId);
-//       document.title = ``;
-//       document.title = `smartCINEMA | HOME`;
-
-//   } else {
-//        stateId = event.state.id;
-//       select_tab(stateId);
-//         document.title = ``;
-//         document.title = `smartCINEMA | ${stateId.toUpperCase()}`;
-//   }
-// });
