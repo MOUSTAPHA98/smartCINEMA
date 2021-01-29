b7f328575b89a24abe29a6a351d226352dc7ce9b
MOUSTAPHA98 <67459997+MOUSTAPHA98@users.noreply.github.com>
2021-01-29 11:10:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ console.log(pathLink);
 var load_content = new Function (`return GET_DATA_${pathLink}()`);
 
 // Load Default HomePage App Content
+(pathLink.substring(0,6) == "movie/" ? load_content = new Function (`return GET_DATA_movie()`) : null)
+load_content();
+
+
 (pathLink == "" || pathLink == "index.html" ? load_content = new Function (`return GET_DATA_home()`) : null);
 load_content();
 
- (pathLink.substring(0,6) == "movie/" ? load_content = new Function (`return GET_DATA_movie()`) : null)
- load_content();
 
 
 
